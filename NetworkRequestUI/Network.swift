ad5a1e4b06a19d63a30187fd4a10c83a3b0344e6
Iurie Guzun <iurie_guzun@yahoo.com>
2022-02-04 12:17:12 -0500

    User file created

--- NetworkRequestUI/Network.swift
+++ NetworkRequestUI/Network.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+import SwiftUI
+
+class Network: ObservableObject {
+
+}
---
+++ NetworkRequestUI/User.swift
@@ -0,0 +1,38 @@
+//
+//  User.swift
+//  NetworkRequestUI
+//
+//  Created by Iurie Guzun on 2022-02-04.
+//
+
+import Foundation
+
+struct User: Identifiable, Decodable {
+    var id: Int
+    var name: String
+    var username: String
+    var email: String
+    var address: Address
+    var phone: String
+    var website: String
+    var company: Company
+
+    struct Address: Decodable {
+        var street: String
+        var suite: String
+        var city: String
+        var zipcode: String
+        var geo: Geo
+
+        struct Geo: Decodable {
+            var lat: String
+            var lng: String
+        }
+    }
+
+    struct Company: Decodable {
+        var name: String
+        var catchPhrase: String
+        var bs: String
+    }
+}
