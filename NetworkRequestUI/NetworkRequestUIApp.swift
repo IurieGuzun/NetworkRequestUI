4d5e592bcf4c83a6ed1a965c957881972962da15
Iurie Guzun <iurie_guzun@yahoo.com>
2022-02-04 12:22:25 -0500

    NetworkRequestUIApp file modified

--- NetworkRequestUI/NetworkRequestUIApp.swift
+++ NetworkRequestUI/NetworkRequestUIApp.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 @main
 struct NetworkRequestUIApp: App {
+    
+    var network = Network()  // Added by Iurie
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(network) // Added by Iurie
+            
         }
     }
 }
